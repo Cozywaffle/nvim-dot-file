cb7494e875bb34942fc3ec44c344edfa0b3f5392
QuocToan259 <toaneras13@gmail.com>
2024-09-02 11:32:20 +0700

    configuration

--- after/plugin/presence.rc.lua
+++ after/plugin/presence.rc.lua
@@ -1,18 +1,21 @@
-local status, presence = pcall (require, 'presence')
+local status, presence = pcall(require, 'presence')
 if (not status) then return end
 
 presence.setup {
-  auto_update = true,
-  neovim_image_text = "Neovim",
-  main_image = "file",
-  client_id = "793271441293967371",
-  log_level = nil,
-  file_assets = {
-          js = { "Javascript", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNCTzPO12FHETdFvWKKYuxBGO8YgBFY6oMlQ&s" }
-  },
-  show_time = true,
-  buttons = true,
-  editing_text = "Editing %s",
-  reading_text = "Reading %s",
-  workspace_text = "Working on %s"
+        auto_update = true,
+        neovim_image_text = "Neovim",
+        main_image = "file",
+        client_id = "793271441293967371",
+        log_level = nil,
+        file_assets = {
+                js = { "Javascript", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNCTzPO12FHETdFvWKKYuxBGO8YgBFY6oMlQ&s" }
+        },
+        show_time = true,
+        buttons = true,
+
+        editing_text = "📝 Editing %s",
+        file_explorer_text = "Browsing %s",
+        reading_text = "Reading %s",
+        workspace_text = "Working on %s",
+        git_commit_text = "Commiting changes"
 }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,26 @@
 local status, packer = pcall(require, 'packer')
 if (not status) then
-  print("Packer is not installed")
-  return
+        print("Packer is not installed")
+        return
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-        'svrana/neosolarized.nvim',
-        requires = { 'tjdevries/colorbuddy.nvim' }
-  }
-  use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use 'andweeb/presence.nvim'
-  use 'neovim/nvim-lspconfig'
-end) 
+        use 'wbthomason/packer.nvim'
+        use {
+                'svrana/neosolarized.nvim',
+                requires = { 'tjdevries/colorbuddy.nvim' }
+        }
+        use {
+                'nvim-lualine/lualine.nvim',
+                requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+        }
+        use 'andweeb/presence.nvim'
+        use 'neovim/nvim-lspconfig'
+        use 'onsails/lspkind-nvim'
+        use 'L3MON4D3/LuaSnip'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'hrsh7th/nvim-cmp'
+end)
